3a6eae64387dc9eb9bc7db278d99057b451291b9
wandabastyle <wandabastyle@gmail.com>
2025-10-24 18:58:26 +0200

    Update configuration.nix: Added Timezone; Local & zRAM

--- configuration.nix
+++ configuration.nix
@@ -6,8 +6,23 @@
 		./modules/regreet.nix
 	];
 
+	# Timezone & Locale
+	time.timeZone = "Europe/Berlin";
+	i18n.defaultLocale = "de_DE.UTF-8";
+	i18n.supportLocales = " [
+		"de_DE.UTF-8/UTF-8"
+		"en_US.UTF-8/UTF-8"
+	];
+
+	# Bootloader (EFI)
   boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
+
+	# zRAM
+	zramSwap {
+		enable = true;
+		memoryPercent = 50;
+	};
 
 	environment.systemPackages = with pkgs; [
 		vim
@@ -24,4 +39,5 @@
 	nix.settings.experimental-features = [ "nix-command" "flakes"];
 	system.stateVersion = "25.05";
 }
+
 
