03bc122752162df00eb8497fcf1d47ef01d86dc4
wandabastyle <wandabastyle@gmail.com>
2025-10-31 11:51:12 +0100

    Update asus-amd.nix: Added ROCm

--- modules/asus-amd.nix
+++ modules/asus-amd.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
   # ASUS/AMD bits
   services.asusd.enable = true;
   services.asusd.enableUserService = true;
@@ -6,4 +6,12 @@
 
   # Use the latest kernel from nixpkgs (good for new GPUs/laptops)
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Add ROCm OpenCL to whatever hardware.graphics is already setting
+  hardware.graphics.extraPackages = lib.mkAfter [
+    pkgs.rocmPackages.clr.icd
+  ];
+
+  # (Optional tools)
+  # environment.systemPackages = with pkgs; [ rocm-smi vulkan-tools glxinfo ];
 }
