e7569c4e9befa342a925b529c11194b062a628c1
wandabastyle <wandabastyle@gmail.com>
2025-10-31 11:40:39 +0100

    Use the latest kernel from your nixpkgs

--- modules/asus-amd.nix
+++ modules/asus-amd.nix
@@ -1,6 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
+  # ASUS/AMD bits
   services.asusd.enable = true;
   services.asusd.enableUserService = true;
-
   services.supergfxd.enable = true;
+
+  # Use the latest kernel from nixpkgs (good for new GPUs/laptops)
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
