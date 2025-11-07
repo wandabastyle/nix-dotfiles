5661e070985b970603fcf3649998fcf0ce72ccb1
kanashi <kanashi@me.com>
2025-11-07 22:36:50 +0100

    Added disko for loq15arp9

---
+++ disko/loq15arp9-disko.nix
@@ -0,0 +1,45 @@
+{
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      # Prefer stable by-id here
+      device = "/dev/disk/by-id/nvme-SOLIDIGM_SSDPFINW010TZL_BTEH425204151P0E";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "512M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "fmask=0077" "dmask=0077" ];
+            };
+          };
+
+          root = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-L" "nixos" ];
+              subvolumes = {
+                "@root".mountpoint = "/";
+                "@home".mountpoint = "/home";
+                "@nix".mountpoint  = "/nix";
+                "@log".mountpoint  = "/var/log";
+                "@snapshots".mountpoint = "/.snapshots";
+                # Optional to keep snapshots lean:
+                "@var".mountpoint  = "/var";
+                "@tmp".mountpoint  = "/var/tmp";
+                "@games".mountpoint     = "/games";
+              };
+              # Common FS mount options applied to all subvols:
+              mountOptions = [ "compress=zstd" "noatime" "ssd" "space_cache=v2" ];
+            };
+          };
+        };
+      };
+    };
+  };
+}
