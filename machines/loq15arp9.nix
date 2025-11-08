a455334315cc6101cca2bc93182a08c829c3b6a9
kanashi <kanashi@me.com>
2025-11-08 15:30:55 +0100

    Cleanup disko

--- machines/loq15arp9.nix
+++ machines/loq15arp9.nix
@@ -13,62 +13,6 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/d890ee3d-9cb1-48a2-b2d5-04d8dd8257eb";
-      fsType = "btrfs";
-      options = [ "subvol=@root" ];
-    };
-
-  fileSystems."/.snapshots" =
-    { device = "/dev/disk/by-uuid/d890ee3d-9cb1-48a2-b2d5-04d8dd8257eb";
-      fsType = "btrfs";
-      options = [ "subvol=@snapshots" ];
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/FDDB-1B68";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  fileSystems."/games" =
-    { device = "/dev/disk/by-uuid/d890ee3d-9cb1-48a2-b2d5-04d8dd8257eb";
-      fsType = "btrfs";
-      options = [ "subvol=@games" ];
-    };
-
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/d890ee3d-9cb1-48a2-b2d5-04d8dd8257eb";
-      fsType = "btrfs";
-      options = [ "subvol=@home" ];
-    };
-
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/d890ee3d-9cb1-48a2-b2d5-04d8dd8257eb";
-      fsType = "btrfs";
-      options = [ "subvol=@nix" ];
-    };
-
-  fileSystems."/var" =
-    { device = "/dev/disk/by-uuid/d890ee3d-9cb1-48a2-b2d5-04d8dd8257eb";
-      fsType = "btrfs";
-      options = [ "subvol=@var" ];
-    };
-
-  fileSystems."/var/log" =
-    { device = "/dev/disk/by-uuid/d890ee3d-9cb1-48a2-b2d5-04d8dd8257eb";
-      fsType = "btrfs";
-      options = [ "subvol=@log" ];
-    };
-
-  fileSystems."/var/tmp" =
-    { device = "/dev/disk/by-uuid/d890ee3d-9cb1-48a2-b2d5-04d8dd8257eb";
-      fsType = "btrfs";
-      options = [ "subvol=@tmp" ];
-    };
-
-  swapDevices = [ ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
